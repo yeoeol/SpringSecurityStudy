848341681a561ffe42b9f0c136ff216f5a46555c
yeoeol <gnsl3528@naver.com>
2025-01-09 23:13:37 +0900

    회원 중복 검증 및 처리 로직

--- src/main/java/com/example/testsecurity/entity/UserEntity.java
+++ src/main/java/com/example/testsecurity/entity/UserEntity.java
@@ -1,5 +1,6 @@
 package com.example.testsecurity.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class UserEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	@Column(unique = true)
 	private String username;
 	private String password;
 
--- src/main/java/com/example/testsecurity/service/JoinService.java
+++ src/main/java/com/example/testsecurity/service/JoinService.java
@@ -18,6 +18,10 @@ public class JoinService {
 
 	public void joinProcess(JoinDTO joinDTO) {
 		// db에 이미 동일한 username을 가진 회원이 존재하는지?
+		boolean isUser = userRepository.existsByUsername(joinDTO.getUsername());
+		if (isUser) {
+			return;
+		}
 
 		UserEntity data = new UserEntity();
 
