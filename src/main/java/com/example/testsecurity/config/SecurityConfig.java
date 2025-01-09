20b7dd99a5ca9acf66f6c0e0b02b25d5cfc3ff26
yeoeol <gnsl3528@naver.com>
2025-01-09 23:48:48 +0900

    세션 설정(소멸, 중복 로그인, 고정 보호)

--- src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -37,6 +37,16 @@ public class SecurityConfig {
 		http
 			.csrf((auth) -> auth.disable());
 
+		// 다중 로그인 설정
+		http
+			.sessionManagement((auth) -> auth
+				.maximumSessions(1)	// 하나의 아이디에 대한 다중 로그인 허용 개수
+				.maxSessionsPreventsLogin(true));	// 다중 로그인 개수를 초과하였을 경우 처리 방법, - true : 초과시 새로운 로그인 차단 ; - false : 초과시 기존 세션 하나 삭제
+
+		// 세션 고정 보호
+		http
+			.sessionManagement((auth) -> auth
+				.sessionFixation().changeSessionId());
 		return http.build();
 	}
 }
