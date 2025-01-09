4d47f13d1a65c48e37895117bfff4403b16793d6
yeoeol <gnsl3528@naver.com>
2025-01-09 23:13:37 +0900

    회원 중복 검증 및 처리 로직

--- src/main/java/com/example/testsecurity/repository/UserRepository.java
+++ src/main/java/com/example/testsecurity/repository/UserRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.testsecurity.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+	boolean existsByUsername(String username);
 }
