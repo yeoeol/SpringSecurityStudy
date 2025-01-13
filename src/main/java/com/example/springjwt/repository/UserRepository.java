9bd37e5d2d578c89d5013358dd0cb321d3702c14
yeoeol <gnsl3528@naver.com>
2025-01-13 22:22:45 +0900

    회원가입 로직 구현

--- src/main/java/com/example/springjwt/repository/UserRepository.java
+++ src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.springjwt.entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
-
+	boolean existsByUsername(String username);
 }
