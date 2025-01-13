eec0f81b7b9ccd193646b872884f19efdb378994
yeoeol <gnsl3528@naver.com>
2025-01-13 21:54:45 +0900

    DB연결 및 Entity 작성

---
+++ src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.springjwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springjwt.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+}
