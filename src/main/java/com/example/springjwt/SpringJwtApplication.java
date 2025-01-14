879207d682750f3b9ec441a65b3a3e8f9c549e14
yeoeol <gnsl3528@naver.com>
2025-01-15 01:05:27 +0900

    JWT 발급 및 검증 클래스

--- src/main/java/com/example/springjwt/SpringJwtApplication.java
+++ src/main/java/com/example/springjwt/SpringJwtApplication.java
@@ -1,5 +1,7 @@
 package com.example.springjwt;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJwtApplication {
 
 	public static void main(String[] args) {
+		System.out.println(new Date());
 		SpringApplication.run(SpringJwtApplication.class, args);
 	}
 
