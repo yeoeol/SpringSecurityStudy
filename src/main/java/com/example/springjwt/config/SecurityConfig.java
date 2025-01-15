0f36246a21dec0e7838b1df2ec120d4baa221b0c
yeoeol <gnsl3528@naver.com>
2025-01-15 21:06:00 +0900

    CORS 설정

--- src/main/java/com/example/springjwt/config/SecurityConfig.java
+++ src/main/java/com/example/springjwt/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.springjwt.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,11 +12,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.example.springjwt.jwt.JWTFilter;
 import com.example.springjwt.jwt.JWTUtil;
 import com.example.springjwt.jwt.LoginFilter;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -36,6 +41,31 @@ public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http
+			.cors((cors) -> cors
+				.configurationSource(new CorsConfigurationSource() {
+					@Override
+					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+						CorsConfiguration configuration = new CorsConfiguration();
+
+						// 허용할 프론트엔드 쪽에서 데이터를 받기 위해 3000번 포트 허용
+						configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+						// 허용할 메소드 설정
+						configuration.setAllowedMethods(Collections.singletonList("*"));
+						// 프론트엔드에서 credential을 설정하면 true로 바꿔줘야 함
+						configuration.setAllowCredentials(true);
+						// 허용할 헤더
+						configuration.setAllowedHeaders(Collections.singletonList("*"));
+						// 허용할 시간
+						configuration.setMaxAge(3600L);
+
+						// 우리 쪽에서 프론트엔드(클라이언트 단)로 헤더를 보내줄 때 Authorization 헤더도 허용해 주어야 함
+						configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+						return configuration;
+					}
+				}));
 
 		//csrf disable
 		http
