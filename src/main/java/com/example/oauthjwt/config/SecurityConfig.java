ab959a38470c2ead55481c9d004be647a6bf8ec7
yeoeol <gnsl3528@naver.com>
2025-01-17 00:18:45 +0900

    OAuth2UserService 응답 받기

--- src/main/java/com/example/oauthjwt/config/SecurityConfig.java
+++ src/main/java/com/example/oauthjwt/config/SecurityConfig.java
@@ -2,15 +2,22 @@ package com.example.oauthjwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.oauthjwt.service.CustomOAuth2UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+	private final CustomOAuth2UserService customOAuth2UserService;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		//csrf disable
@@ -27,7 +34,9 @@ public class SecurityConfig {
 
 		//oauth2
 		http
-			.oauth2Login(Customizer.withDefaults());
+			.oauth2Login((oauth2) -> oauth2
+				.userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+					.userService(customOAuth2UserService))));
 
 		//경로별 인가 작업
 		http
