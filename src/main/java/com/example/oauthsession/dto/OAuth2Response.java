bc6a5bf281d7533ccf35e7c0d36b32924920ef9b
yeoeol <gnsl3528@naver.com>
2025-01-18 01:30:19 +0900

    basic setting

---
+++ src/main/java/com/example/oauthsession/dto/OAuth2Response.java
@@ -0,0 +1,12 @@
+package com.example.oauthsession.dto;
+
+public interface OAuth2Response {
+
+    String getProvider();
+
+    String getProviderId();
+
+    String getEmail();
+
+    String getName();
+}
---
+++ src/main/java/com/example/oauthsession/service/CustomOAuth2UserService.java
@@ -0,0 +1,74 @@
+package com.example.oauthsession.service;
+
+import com.example.oauthsession.dto.CustomOAuth2User;
+import com.example.oauthsession.dto.GoogleResponse;
+import com.example.oauthsession.dto.NaverResponse;
+import com.example.oauthsession.dto.OAuth2Response;
+import com.example.oauthsession.entity.UserEntity;
+import com.example.oauthsession.repository.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println(oAuth2User.getAttributes());
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+        if (registrationId.equals("naver")) {
+
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+        else if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+        }
+        else {
+
+            return null;
+        }
+
+
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        UserEntity existData = userRepository.findByUsername(username);
+
+        String role = "ROLE_USER";
+        if (existData == null) {
+
+            UserEntity userEntity = new UserEntity();
+
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setRole(role);
+
+            userRepository.save(userEntity);
+        }
+        else {
+
+            existData.setUsername(username);
+            existData.setEmail(oAuth2Response.getEmail());
+
+            role = existData.getRole();
+
+            userRepository.save(existData);
+        }
+
+        return new CustomOAuth2User(oAuth2Response, role);
+    }
+}
