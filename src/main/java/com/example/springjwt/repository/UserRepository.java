3553b2934f57b82ad8a58d07bd013621341485aa
yeoeol <gnsl3528@naver.com>
2025-01-20 21:57:57 +0900

    basic setting

---
+++ src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.springjwt.repository;
+
+import com.example.springjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
+}
