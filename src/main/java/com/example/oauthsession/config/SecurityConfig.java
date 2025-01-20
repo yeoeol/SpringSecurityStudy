455c4cbcf2c63e432237444e43315b49840a78ba
yeoeol <gnsl3528@naver.com>
2025-01-20 19:50:02 +0900

    커스텀 로그인 페이지

--- src/main/java/com/example/oauthsession/config/SecurityConfig.java
+++ src/main/java/com/example/oauthsession/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
 
         http
                 .oauth2Login((oauth2) -> oauth2
+                        .loginPage("/login")
                         .userInfoEndpoint((userInfoEndpointConfig) ->
                                 userInfoEndpointConfig.userService(customOAuth2UserService)));
 
