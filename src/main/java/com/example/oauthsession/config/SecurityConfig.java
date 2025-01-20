d50e6dfbc14937ad7143cfcfbe635ed836036ee8
yeoeol <gnsl3528@naver.com>
2025-01-20 20:51:43 +0900

    OAuth2AuthorizedClientService

--- src/main/java/com/example/oauthsession/config/SecurityConfig.java
+++ src/main/java/com/example/oauthsession/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.example.oauthsession.config;
 
 import com.example.oauthsession.oauth2.CustomClientRegistrationRepo;
+import com.example.oauthsession.oauth2.CustomOAuth2AuthorizedClientService;
 import com.example.oauthsession.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,6 +20,8 @@ public class SecurityConfig {
 
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomClientRegistrationRepo customClientRegistrationRepo;
+    private final CustomOAuth2AuthorizedClientService customOAuth2AuthorizedClientService;
+    private final JdbcTemplate jdbcTemplate;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,6 +39,7 @@ public class SecurityConfig {
                 .oauth2Login((oauth2) -> oauth2
                         .loginPage("/login")
                         .clientRegistrationRepository(customClientRegistrationRepo.clientRegistrationRepository())
+                        .authorizedClientService(customOAuth2AuthorizedClientService.oAuth2AuthorizedClientService(jdbcTemplate, customClientRegistrationRepo.clientRegistrationRepository()))
                         .userInfoEndpoint((userInfoEndpointConfig) ->
                                 userInfoEndpointConfig.userService(customOAuth2UserService)));
 
